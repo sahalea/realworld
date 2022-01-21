0c711c154770878e237ed258e47f5ab685ee7000
sahel <sahal.ea@gmail.com>
2022-01-21 16:41:52 +0400

    Date Filter

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# realworld
+# real world
+
+This project was vite with Vue
+
+## Available Scripts
+
+In the project directory, you can run:
+### `npm install`
+
+Install all dependencies
+
+### `npm run dev`
+
+Runs the app in the development mode.\
+Open [http://localhost:4000](http://localhost:4000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm run build`
+
+For build application
+
--- src/component/CardList.vue
+++ src/component/CardList.vue
@@ -3,13 +3,17 @@
     <div class="feed-card border-b px-2 py-3">
       <div class="feed-card-header flex items-center">
         <el-avatar :src="item.author.image" size="20" class="mr-3">
-          <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png" />
+          <img
+            src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"
+          />
         </el-avatar>
         <div class="flex flex-col flex-1">
           <h1>
-            <router-link :to="myArticleHref(item)">{{ item.author.username }}</router-link>
+            <router-link :to="myArticleHref(item)">
+              {{ item.author.username }}
+            </router-link>
           </h1>
-          <p class="text-gray-400 text-xs">{{ item.createdAt }}</p>
+          <p class="text-gray-400 text-xs">{{ filterDate(item.createdAt) }}</p>
         </div>
         <div class="feed-card-likes rounded-md px-3 py-1">
           <i></i>
@@ -25,28 +29,42 @@
       <div class="flex items-center">
         <a href="" class="flex-1 text-gray-400 text-sm">Read more...</a>
         <div class="feed-card-tags flex items-center flex-wrap">
-          <el-tag v-for="tag in item.tagList" :key="tag" class="ml-2" type="info">{{ tag }}</el-tag>
+          <el-tag
+            v-for="tag in item.tagList"
+            :key="tag"
+            class="ml-2"
+            type="info"
+          >
+            {{ tag }}
+          </el-tag>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
+import moment from 'moment';
 
 export default defineComponent({
   props: {
-    articles: [Array]
+    articles: [Array],
   },
   setup() {},
   methods: {
+    /**
+     * Filter the date
+     */
+    filterDate(date) {
+      return moment(date).format('DD-MM-YYYY HH:MM');
+    },
     myArticleHref(row) {
       return `/article/${row.author.username}`;
     },
     navigate(item) {
       this.$router.push(`/article/detail/${item.slug}`);
-    }
-  }
+    },
+  },
 });
 </script>
 
